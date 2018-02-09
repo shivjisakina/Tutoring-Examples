69c993f31924c97a435933a77866063f3f7e14df
Sakina Shivji <shivjisakina@gmail.com>
2018-02-08 22:08:20 -0500

    pushing changes

--- Josh/app.js
+++ Josh/app.js
@@ -1,6 +1,24 @@
-console.log("hello world");
+// console.log("hello world");
 
+var object = {
+    name: "Josh",
+    college: "UCF"
+}
 
+for (var key in object) {
+    var value = object[key];
+    // console.log(key, value)
+
+    if (object.hasOwnProperty(key)) {
+        console.log(key, object, value)
+    }
+
+}
+
+/*if (buz.hasOwnProperty(name)) {
+ console.log('this is fog (' +
+ name + ') for sure. Value: ' + buz[name]);
+ }*/
 
 
 
